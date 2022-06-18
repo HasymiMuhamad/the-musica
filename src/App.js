b614f31f6d78337c285723d65781b7ab6e9c8815
Muhamad Hasymi Hilman <hasymimuhamad@gmail.com>
2022-06-18 22:12:06 +0700

    rollback test ssh

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
   return (
     <div className="app">
       <QueryClientProvider client={queryClient}>
-        <p>test coba</p>
         <Player />
       </QueryClientProvider>
     </div>
